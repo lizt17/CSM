e48a115e80daa7d00aa1f8c7f420d7b92b20d24d
Accelerator <903558062@qq.com>
2022-05-07 16:33:17 +0800

    Vumat template

---
+++ vumat-hypoelastic.f
@@ -0,0 +1,50 @@
+C Vumat - Hypoelastic material
+C Green - Naghdi rate
+C
+C
+C lizt21 - 2022-05-07 
+
+      subroutine vumat(
+C Read only (unmodifiable)variables -
+     1  nblock, ndir, nshr, nstatev, nfieldv, nprops, lanneal,
+     2  stepTime, totalTime, dt, cmname, coordMp, charLength,
+     3  props, density, strainInc, relSpinInc,
+     4  tempOld, stretchOld, defgradOld, fieldOld,
+     5  stressOld, stateOld, enerInternOld, enerInelasOld,
+     6  tempNew, stretchNew, defgradNew, fieldNew,
+C Write only (modifiable) variables -
+     7  stressNew, stateNew, enerInternNew, enerInelasNew )
+C
+      include 'vaba_param.inc'
+      implicit none
+C
+      dimension props(nprops), density(nblock), coordMp(nblock,*),
+     1  charLength(nblock), strainInc(nblock,ndir+nshr),
+     2  relSpinInc(nblock,nshr), tempOld(nblock),
+     3  stretchOld(nblock,ndir+nshr),
+     4  defgradOld(nblock,ndir+nshr+nshr),
+     5  fieldOld(nblock,nfieldv), stressOld(nblock,ndir+nshr),
+     6  stateOld(nblock,nstatev), enerInternOld(nblock),
+     7  enerInelasOld(nblock), tempNew(nblock),
+     8  stretchNew(nblock,ndir+nshr),
+     8  defgradNew(nblock,ndir+nshr+nshr),
+     9  fieldNew(nblock,nfieldv),
+     1  stressNew(nblock,ndir+nshr), stateNew(nblock,nstatev),
+     2  enerInternNew(nblock), enerInelasNew(nblock)
+C
+      character*80 cmname
+C
+
+      do 100 km = 1,nblock
+
+
+
+
+
+
+
+        user coding
+  100 continue
+
+      return
+      end
